b6c62d5c98a842de0f1683a8836f22886ea36eb5
KangBoSeok <kbs9901@gmail.com>
2024-10-22 11:49:32 +0900

    Feat: 주문내역 기능

--- dearpet/src/App.js
+++ dearpet/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import { Routes, Route, Router } from 'react-router-dom';
 import Login from './pages/Login'; // 로그인 컴포넌트
 import SignUp from './pages/SignUp'; // 회원가입 컴포넌트
+import OrderHistory from './pages/OrderHistory';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/orders" element={<OrderHistory/>}/>
     </Routes>
   );
 };

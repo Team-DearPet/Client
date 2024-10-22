9e51594691d130fdc9372b17993ff1cdcd1a8e33
wldusdn <wldusdngg@naver.com>
2024-10-22 10:11:28 +0900

    merge: develop에 merge

--- dearpet/src/pages/Login.jsx
+++ dearpet/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -6,10 +7,8 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { FormGroup } from '@mui/material';
-import './SignUp.css';
+import '../style/Login.css';  // 스타일을 위한 CSS 파일
+
 // Material UI 테마
 const theme = createTheme({
   typography: {
@@ -33,72 +32,84 @@ const theme = createTheme({
           borderColor: '#000', // outlined 버튼 테두리 색상을 검은색으로 설정
           '&:hover': {
             borderColor: '#333', // outlined 버튼 hover 시 테두리 색상 변경
-            backgroundColor: '#F5F5F5', // hover 시 배경색 변경
+            backgroundColor: '#f5f5f5', // hover 시 배경색 변경
           },
         },
       },
     },
   },
 });
-export default function SignUp() {
+
+export default function Login({ setIsLoggedIn, setUserId }) {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [nickname, setNickname] = useState("")
+  const [errorMessage, setErrorMessage] = useState(""); // 에러 메시지 상태 추가
+
+  // 로컬 로그인 처리
   const handleSubmit = async (event) => {
     event.preventDefault();
+    
     const userData = {
       username: id,
       password: password,
-      email: email,
-      nickname: nickname
     };
+
     try {
-      const response = await fetch("http://localhost:8080/api/users/signup", {
+      const response = await fetch("http://localhost:8080/api/users/login", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(userData),
       });
+
+      const textResponse = await response.text(); 
+
       if (response.ok) {
-        const result = await response.json();
-        console.log('User created:', result);
-        alert("회원가입이 완료됐습니다!")
-        window.location.href = "/login";
+        const token = textResponse;  // JWT 토큰 문자열 처리
+        console.log('Login successful:', token);
+        localStorage.setItem('token', token);  // 로그인 토큰 저장
+        localStorage.setItem('isLoggedIn', true);  // 로그인 상태 저장
+        localStorage.setItem('userId', id);
+        setIsLoggedIn(true);  // 상태 업데이트
+
       } else {
-        console.error('Error creating user');
+        console.error('Login failed:', textResponse);
       }
+      
     } catch (error) {
       console.error('Error:', error);
     }
   };
+
+
+
   return (
     <div className="main-content">
-        <div className="dearpet-title">
+      <div className="dearpet-title" style={{fontFamily:"Licorice", fontSize:"100px"}}>
         DearPet
-        </div>
+      </div>
       <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs" className="signup-container">
+        <Container component="main" maxWidth="xs" className="login-container">
           <CssBaseline />
           <Box
             sx={{
               marginTop: 4,
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'center', // 중앙 정렬
+              alignItems: 'center', // 가운데 정렬
             }}
           >
-            <Typography
+            <Typography style={{ textShadow: "none" }}
               component="h1"
               variant="h5"
               sx={{ marginBottom: 3, textAlign: 'center' }}
             >
-              회원가입
+              로그인
             </Typography>
             <Typography
               variant="body1"
-              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
+              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold', textShadow: 'none' }}
             >
               아이디
             </Typography>
@@ -106,12 +117,15 @@ export default function SignUp() {
               margin="normal"
               required
               fullWidth
-              id="id"
-              name="id"
-              autoComplete="id"
+              id="username"
+              name="username"
+              autoComplete="username"
               autoFocus
               value={id}
               onChange={(e) => setId(e.target.value)}
+              InputProps={{
+                disableUnderline: true,
+              }}
               sx={{
                 mb: 2,
                 '& .MuiInputBase-root': {
@@ -119,14 +133,14 @@ export default function SignUp() {
                   borderRadius: '5px',
                   padding: '10px',
                   '&:focus': {
-                    borderColor: '#B3D9E2',
+                    borderColor: '#7B52E1',
                   },
-                },
-              }}
-            />
+                }}}
+              />
+
             <Typography
               variant="body1"
-              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
+              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold', textShadow: 'none' }}
             >
               비밀번호
             </Typography>
@@ -140,6 +154,9 @@ export default function SignUp() {
               autoComplete="current-password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              InputProps={{
+                disableUnderline: true,
+              }}
               sx={{
                 mb: 2,
                 '& .MuiInputBase-root': {
@@ -147,78 +164,64 @@ export default function SignUp() {
                   borderRadius: '5px',
                   padding: '10px',
                   '&:focus': {
-                    borderColor: '#B3D9E2',
+                    borderColor: '#7B52E1',
                   },
                 },
               }}
             />
-            <Typography
-              variant="body1"
-              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
-            >
-              닉네임
-            </Typography>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="nickname"
-              type="nickname"
-              id="nickname"
-              autoComplete="nickname"
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)}
-              sx={{
-                mb: 2,
-                '& .MuiInputBase-root': {
-                  border: '1px solid #ccc',
-                  borderRadius: '5px',
-                  padding: '10px',
-                  '&:focus': {
-                    borderColor: '#B3D9E2',
-                  },
-                },
-              }}
-            />
-            <Typography
-              variant="body1"
-              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
-            >
-              이메일
-            </Typography>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="email"
-              type="email"
-              id="email"
-              autoComplete="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              sx={{
-                mb: 2,
-                '& .MuiInputBase-root': {
-                  border: '1px solid #ccc',
-                  borderRadius: '5px',
-                  padding: '10px',
-                  '&:focus': {
-                    borderColor: '#B3D9E2',
-                  },
-                },
-              }}
-            />
+            
+            {/* 로컬 로그인 버튼 */}
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-              className="signup-button"
+              className="login-button"
               onClick={handleSubmit}
-              style={{backgroundColor:'#7B52E1'}}
+              style={{backgroundColor:"#7B52E1"}}
             >
-              회원가입
+              로그인
             </Button>
+
+            {/* Google 로그인 버튼 */}
+            <Button
+              fullWidth
+              variant="outlined"
+              startIcon={<img
+                className='icon-image'
+                src={require('../images/Google.jpg')}
+                alt="Google"
+                style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+              />}
+              sx={{ mt: 2, mb: 2 }}
+            >
+              구글로 로그인
+            </Button>
+
+            {/* Kakao 로그인 버튼 */}
+            <Button
+              fullWidth
+              variant="outlined"
+              startIcon={<img
+              className='icon-image'
+                src={require('../images/kakao.jpg')}
+                alt="kakao"
+                style={{ width: '24px', height: '24px', borderRadius: '50%' }}
+              />}
+              sx={{ mt: 2, mb: 2 }}
+            >
+              카카오톡으로 로그인
+            </Button>
+            <Typography
+              variant="body2"
+              align="center"
+              sx={{ mt: 2 }}
+            >
+              회원가입하지 않으셨나요?{" "}
+              <Link to="/signup" style={{ textDecoration: 'none', color: '#7B52E1' }}>
+                회원가입하러가기
+              </Link>
+            </Typography>
           </Box>
         </Container>
       </ThemeProvider>
--- dearpet/src/pages/SignUp.jsx
+++ dearpet/src/pages/SignUp.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -7,7 +6,10 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import './Login.css';  // 스타일을 위한 CSS 파일
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { FormGroup } from '@mui/material';
+import '../style/SignUp.css';
 
 // Material UI 테마
 const theme = createTheme({
@@ -32,84 +34,72 @@ const theme = createTheme({
           borderColor: '#000', // outlined 버튼 테두리 색상을 검은색으로 설정
           '&:hover': {
             borderColor: '#333', // outlined 버튼 hover 시 테두리 색상 변경
-            backgroundColor: '#f5f5f5', // hover 시 배경색 변경
+            backgroundColor: '#F5F5F5', // hover 시 배경색 변경
           },
         },
       },
     },
   },
 });
-
-export default function Login({ setIsLoggedIn, setUserId }) {
+export default function SignUp() {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(""); // 에러 메시지 상태 추가
-
-  // 로컬 로그인 처리
+  const [email, setEmail] = useState("");
+  const [nickname, setNickname] = useState("")
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
     const userData = {
       username: id,
       password: password,
+      email: email,
+      nickname: nickname
     };
-
     try {
-      const response = await fetch("http://localhost:8080/api/users/login", {
+      const response = await fetch("http://localhost:8080/api/users/signup", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(userData),
       });
-
-      const textResponse = await response.text(); 
-
       if (response.ok) {
-        const token = textResponse;  // JWT 토큰 문자열 처리
-        console.log('Login successful:', token);
-        localStorage.setItem('token', token);  // 로그인 토큰 저장
-        localStorage.setItem('isLoggedIn', true);  // 로그인 상태 저장
-        localStorage.setItem('userId', id);
-        setIsLoggedIn(true);  // 상태 업데이트
-
+        const result = await response.json();
+        console.log('User created:', result);
+        alert("회원가입이 완료됐습니다!")
+        window.location.href = "/login";
       } else {
-        console.error('Login failed:', textResponse);
+        console.error('Error creating user');
       }
-      
     } catch (error) {
       console.error('Error:', error);
     }
   };
-
-
-
   return (
     <div className="main-content">
-      <div className="dearpet-title" style={{fontFamily:"Licorice", fontSize:"100px"}}>
+        <div className="dearpet-title">
         DearPet
-      </div>
+        </div>
       <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs" className="login-container">
+        <Container component="main" maxWidth="xs" className="signup-container">
           <CssBaseline />
           <Box
             sx={{
               marginTop: 4,
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'center', // 가운데 정렬
+              alignItems: 'center', // 중앙 정렬
             }}
           >
-            <Typography style={{ textShadow: "none" }}
+            <Typography
               component="h1"
               variant="h5"
               sx={{ marginBottom: 3, textAlign: 'center' }}
             >
-              로그인
+              회원가입
             </Typography>
             <Typography
               variant="body1"
-              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold', textShadow: 'none' }}
+              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
             >
               아이디
             </Typography>
@@ -117,15 +107,12 @@ export default function Login({ setIsLoggedIn, setUserId }) {
               margin="normal"
               required
               fullWidth
-              id="username"
-              name="username"
-              autoComplete="username"
+              id="id"
+              name="id"
+              autoComplete="id"
               autoFocus
               value={id}
               onChange={(e) => setId(e.target.value)}
-              InputProps={{
-                disableUnderline: true,
-              }}
               sx={{
                 mb: 2,
                 '& .MuiInputBase-root': {
@@ -133,14 +120,14 @@ export default function Login({ setIsLoggedIn, setUserId }) {
                   borderRadius: '5px',
                   padding: '10px',
                   '&:focus': {
-                    borderColor: '#7B52E1',
+                    borderColor: '#B3D9E2',
                   },
-                }}}
-              />
-
+                },
+              }}
+            />
             <Typography
               variant="body1"
-              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold', textShadow: 'none' }}
+              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
             >
               비밀번호
             </Typography>
@@ -154,9 +141,6 @@ export default function Login({ setIsLoggedIn, setUserId }) {
               autoComplete="current-password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              InputProps={{
-                disableUnderline: true,
-              }}
               sx={{
                 mb: 2,
                 '& .MuiInputBase-root': {
@@ -164,62 +148,78 @@ export default function Login({ setIsLoggedIn, setUserId }) {
                   borderRadius: '5px',
                   padding: '10px',
                   '&:focus': {
-                    borderColor: '#7B52E1',
+                    borderColor: '#B3D9E2',
                   },
                 },
               }}
             />
-            
-            {/* 로컬 로그인 버튼 */}
+            <Typography
+              variant="body1"
+              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
+            >
+              닉네임
+            </Typography>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="nickname"
+              type="nickname"
+              id="nickname"
+              autoComplete="nickname"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+              sx={{
+                mb: 2,
+                '& .MuiInputBase-root': {
+                  border: '1px solid #ccc',
+                  borderRadius: '5px',
+                  padding: '10px',
+                  '&:focus': {
+                    borderColor: '#B3D9E2',
+                  },
+                },
+              }}
+            />
+            <Typography
+              variant="body1"
+              sx={{ alignSelf: 'flex-start', mb: 1, fontWeight: 'bold' }}
+            >
+              이메일
+            </Typography>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="email"
+              type="email"
+              id="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              sx={{
+                mb: 2,
+                '& .MuiInputBase-root': {
+                  border: '1px solid #ccc',
+                  borderRadius: '5px',
+                  padding: '10px',
+                  '&:focus': {
+                    borderColor: '#B3D9E2',
+                  },
+                },
+              }}
+            />
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-              className="login-button"
+              className="signup-button"
               onClick={handleSubmit}
-              style={{backgroundColor:"#7B52E1"}}
+              style={{backgroundColor:'#7B52E1'}}
             >
-              로그인
+              회원가입
             </Button>
-
-            {/* Google 로그인 버튼 */}
-            <Button
-              fullWidth
-              variant="outlined"
-              startIcon={<img
-                src={require('../images/Google.jpg')}
-                alt="Google"
-                style={{ width: '24px', height: '24px', borderRadius: '50%' }}
-              />}
-              sx={{ mt: 2, mb: 2 }}
-            >
-              구글로 로그인
-            </Button>
-
-            {/* Kakao 로그인 버튼 */}
-            <Button
-              fullWidth
-              variant="outlined"
-              startIcon={<img
-                src={require('../images/kakao.jpg')}
-                alt="kakao"
-                style={{ width: '24px', height: '24px', borderRadius: '50%' }}
-              />}
-              sx={{ mt: 2, mb: 2 }}
-            >
-              카카오톡으로 로그인
-            </Button>
-            <Typography
-              variant="body2"
-              align="center"
-              sx={{ mt: 2 }}
-            >
-              회원가입하지 않으셨나요?{" "}
-              <Link to="/signup" style={{ textDecoration: 'none', color: '#7B52E1' }}>
-                회원가입하러가기
-              </Link>
-            </Typography>
           </Box>
         </Container>
       </ThemeProvider>

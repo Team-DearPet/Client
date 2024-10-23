b6fcd96da87bd07d886c3f63c43aef80e9258bcf
wldusdn <wldusdngg@naver.com>
2024-10-23 10:39:24 +0900

    Update: 배경색 App.css에 적용

--- dearpet/src/App.js
+++ dearpet/src/App.js
@@ -7,18 +7,20 @@ import SignUp from './pages/SignUp'; // 회원가입 컴포넌트
 import Mypage from './pages/Mypage';
 import OrderHistory from './pages/OrderHistory';
 import OrderComplete from './pages/OrderComplete';
-import Map from './pages/Map';
+// import Map from './pages/Map';
+import MyPet from './pages/MyPet';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/map" element={<Map />} />
+      {/* <Route path="/map" element={<Map />} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/mypage" element={<Mypage />} />
+      <Route path="/mypet" element={<MyPet />} />
       <Route path="/orders" element={<OrderHistory/>}/>
-      <Route path="/orderscomplete" element={<OrderComplete/>} />  
+      <Route path="/orderscomplete" element={<OrderComplete/>} />
     </Routes>
   );
 };
--- dearpet/src/pages/Mypage.jsx
+++ dearpet/src/pages/Mypage.jsx
@@ -3,9 +3,10 @@ import Header from '../component/Header';
 import UserDetail from '../component/UserDetail';
 import PetList from '../component/PetList';
 import OrderStatus from '../component/OrderStatus';
+
 function Mypage(props) {
     return (
-        <div style={{backgroundColor:"#F7F4FD"}}>
+        <div>
             <Header />
             <h3 style={{ textAlign:'center'}}>마이페이지</h3>
             <UserDetail />

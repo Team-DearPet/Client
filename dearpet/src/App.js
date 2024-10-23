f026993993f39e8efb08d7ce28f2858729b0bb0b
Sanghee Park <tkdgml1241@naver.com>
2024-10-23 10:04:11 +0900

    feat: 상품상세

--- dearpet/src/App.js
+++ dearpet/src/App.js
@@ -4,28 +4,34 @@ import { Routes, Route, Router } from 'react-router-dom';
 import Login from './component/Login'; // 로그인 컴포넌트
 import SignUp from './component/SignUp'; // 회원가입 컴포넌트
 
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+
 const App = () => {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-    </Routes>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Route path="/detail" element={<ProductDetail />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes><div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      </>
   );
 };
 

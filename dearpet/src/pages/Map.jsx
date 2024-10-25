05175246cae0b58879a6f2e6ea4e97d872d69479
KangBoSeok <kbs9901@gmail.com>
2024-10-25 14:31:48 +0900

    UPDATE: 병원찾기 피드백 수정

--- dearpet/src/pages/Map.jsx
+++ dearpet/src/pages/Map.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { GoogleMap, Marker, OverlayView ,InfoWindow, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, Marker, OverlayView, InfoWindow, useJsApiLoader } from "@react-google-maps/api";
 import '../style/Map.css';
-import hospitalIconOpen from '../images/hospital.png'; 
-import hospitalIconClosed from '../images/hospital2.png'; 
+import hospitalIconOpen from '../images/hospital.png';
+import hospitalIconClosed from '../images/hospital2.png';
 import locationIcon from '../images/location.png';
 import Header from "../component/Header";
 import { blue } from "@mui/material/colors";
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import { IconButton } from '@mui/material';
 
 const containerStyle = {
   width: '100%',
@@ -22,6 +24,7 @@ function NearbyAnimalHospitals() {
   const [userLocation, setUserLocation] = useState(null);
   const [hospitalMarkers, setHospitalMarkers] = useState([]);
   const [activeMarker, setActiveMarker] = useState(null);
+  const [copySuccess, setCopySuccess] = useState("");
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
@@ -62,7 +65,7 @@ function NearbyAnimalHospitals() {
               if (status === window.google.maps.places.PlacesServiceStatus.OK) {
                 const isOpen = details.opening_hours ? details.opening_hours.isOpen() : false;
                 const translatedAddress = details.vicinity;
-                const openingHours = details.opening_hours ? details.opening_hours.weekday_text : []; // 영업시간 가져오기
+                const openingHours = details.opening_hours ? details.opening_hours.weekday_text : [];
   
                 resolve({
                   lat: details.geometry.location.lat(),
@@ -72,7 +75,7 @@ function NearbyAnimalHospitals() {
                   website: details.website || '#',
                   phone: details.formatted_phone_number || '정보 없음',
                   isOpen: isOpen,
-                  openingHours: openingHours || [] // 영업시간이 undefined일 경우 빈 배열로 초기화
+                  openingHours: openingHours || []
                 });
               } else {
                 resolve(null);
@@ -88,20 +91,35 @@ function NearbyAnimalHospitals() {
     });
   };
   
-  
-
   const handleMarkerClick = (hospital) => {
     setActiveMarker(hospital);
     const cardElement = document.getElementById(`hospital-card-${hospital.name}`);
     if (cardElement) {
-    cardElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
-  }
+      cardElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
   };
 
   const handleListItemClick = (hospital) => {
     setActiveMarker(hospital);
     setMapCenter({ lat: hospital.lat, lng: hospital.lng });
   };
+
+  const getTodayOpeningHours = (openingHours) => {
+  const today = new Date().getDay(); 
+  const todayHours = openingHours[today - 1]; 
+  const hoursOnly = todayHours?.split(": ")[1]; 
+  return hoursOnly || "정보 없음";
+  };
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text).then(() => {
+      setCopySuccess("복사되었습니다!"); // 복사 성공 시 메시지 표시
+      setTimeout(() => setCopySuccess(""), 2000); // 2초 후 메시지 사라짐
+    }, () => {
+      setCopySuccess("복사 실패!");
+    });
+  };
+
 
   return isLoaded ? (
     <div className="page-container">
@@ -111,7 +129,7 @@ function NearbyAnimalHospitals() {
       </div>
       <div className="map-list-container">
         <div className="map-box">
-          <div className="icon-legend" style={{ position: 'absolute', top: '170px', left: '370px', padding: '10px', border:"none", zIndex: 100 }}>
+          <div className="icon-legend" style={{ position: 'absolute', top: '170px', left: '370px', padding: '10px', border: "none", zIndex: 100 }}>
             <div style={{ display: 'flex', alignItems: 'center', marginBottom: '8px' }}>
               <img src={hospitalIconOpen} alt="진료 중" style={{ width: '20px', height: '20px', marginRight: '8px' }} />
               <span style={{ fontWeight: 'bold' }}>진료 중</span>
@@ -144,7 +162,6 @@ function NearbyAnimalHospitals() {
                 }}
               />
             ))}
-
             {activeMarker && (
               <OverlayView
                 position={{ lat: activeMarker.lat, lng: activeMarker.lng }}
@@ -169,12 +186,12 @@ function NearbyAnimalHospitals() {
                 >
                   <h3 style={{ margin: '0', fontSize: '15px', color: '#333', padding: '5px' }}>{activeMarker.name}</h3>
                 </div>
-                </OverlayView>
+              </OverlayView>
             )}
           </GoogleMap>
         </div>
         <div className="hospital-list">
-        {hospitalMarkers.map((hospital, index) => (
+          {hospitalMarkers.map((hospital, index) => (
             <div 
               key={index} 
               id={`hospital-card-${hospital.name}`} 
@@ -187,20 +204,33 @@ function NearbyAnimalHospitals() {
                   alt="병원 아이콘" 
                   style={{ width: '30px', height: '30px', marginRight: '10px' }} 
                 />
-                <h3 style={{fontWeight:"bold"}}>{hospital.name}</h3>
+                <h3 style={{ fontWeight: "bold" }}>{hospital.name}</h3>
               </div>
-              <p style={{fontWeight:"bold"}}>주소: {hospital.address}</p>
-              <p style={{fontWeight:"bold"}}>전화번호: {hospital.phone}</p>
-              {hospital.openingHours.length > 0 && (
-                <div style={{ fontWeight: 'bold', marginTop: '10px' }}>
-                  <p>영업시간 정보</p>
-                  <ul style={{ paddingLeft: '20px' }}>
-                    {hospital.openingHours.map((hour, i) => (
-                      <li key={i}>{hour}</li>
-                    ))}
-                  </ul>
-                </div>
+              <p style={{ fontWeight: "bold" }}>
+                주소: {hospital.address}
+                <IconButton 
+                  onClick={() => copyToClipboard(hospital.address)} 
+                  size="small" 
+                  style={{ marginLeft: '10px' }}>
+                  <FileCopyIcon fontSize="small" />
+                </IconButton>
+                </p>
+              {hospital.openingHours.length > 0 ? (
+                <p style={{ fontWeight: "bold", marginTop: "10px" }}>
+                  영업시간: {getTodayOpeningHours(hospital.openingHours)}
+                </p>
+              ) : (
+                <p style={{ fontWeight: "bold", marginTop: "10px" }}>영업시간: 정보 없음</p>
               )}
+              <p style={{ fontWeight: "bold" }}>
+                전화번호: {hospital.phone}
+                <IconButton 
+                  onClick={() => copyToClipboard(hospital.phone)} 
+                  size="small" 
+                  style={{ marginLeft: '10px' }}>
+                  <FileCopyIcon fontSize="small" />
+                </IconButton>
+                </p>
               {hospital.website !== '#' && (
                 <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
                   <a
@@ -213,8 +243,7 @@ function NearbyAnimalHospitals() {
                       color: '#fff',
                       textDecoration: 'none',
                       borderRadius: '5px',
-                      fontSize: '12px',
-                      fontWeight: 'bold',
+                      fontSize: '15px',
                       textAlign: 'center',
                     }}
                   >
@@ -230,4 +259,4 @@ function NearbyAnimalHospitals() {
   ) : <div>Loading...</div>;
 }
 
-export default React.memo(NearbyAnimalHospitals);
+export default NearbyAnimalHospitals;

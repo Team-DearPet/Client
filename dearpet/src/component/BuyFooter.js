988159f503a31d9052fb9d816ed1f545b1dd7a8e
Sanghee Park <tkdgml1241@naver.com>
2024-10-25 17:01:51 +0900

    update: css수정, 장바구니 footer연결

--- dearpet/src/component/BuyFooter.js
+++ dearpet/src/component/BuyFooter.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Box, Typography, Button, Divider } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-const BuyFooter = () => {
-  const orderAmount = 30000;  // 총 주문금액
-  const shippingCost = 2500;  // 배송비
-  const totalAmount = orderAmount + shippingCost;  // 결제 예정금액
+const BuyFooter = ({ orderAmount }) => {
+    const shippingCost = 2500;  
+    const totalAmount = orderAmount + shippingCost;  
 
     return (
-        <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 1000 }}>
+        <Box 
+            sx={{ 
+                position: 'fixed', 
+                bottom: 0, 
+                left: 0, 
+                right: 0, 
+                zIndex: 1000 
+            }}>
             <Box
                 sx={{
                     backgroundColor: '#C0ADF0',
@@ -17,33 +23,66 @@ const BuyFooter = () => {
                     justifyContent: 'space-between',
                     alignItems: 'center',
                     borderTop: '1px solid #dee2e6',
-                    color: '#333',
-                }}
-            >
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                    <Typography sx={{ marginRight: '100px', marginLeft: '300px', textAlign: 'center' }}>
+                    color: '#333'
+                }}>
+                <Box 
+                    sx={{ 
+                        display: 'flex', 
+                        alignItems: 'center' 
+                    }}>
+                    <Typography 
+                        sx={{ 
+                            marginRight: '100px', 
+                            marginLeft: '300px', 
+                            textAlign: 'center',
+                            fontSize: '1.2rem' 
+                        }}>
                         총 주문금액 <br />
                         <strong>{orderAmount.toLocaleString()}원</strong>
                     </Typography>
-                    <Divider orientation="vertical" flexItem sx={{ marginRight: '100px', borderColor: '#ddd' }} />
-                    <Typography sx={{ marginRight: '100px', textAlign: 'center' }}>
+                    <Divider 
+                        orientation="vertical" 
+                        flexItem 
+                        sx={{ 
+                            marginRight: '100px', 
+                            borderColor: '#ddd' 
+                        }} 
+                    />
+                    <Typography 
+                        sx={{ 
+                            marginRight: '100px', 
+                            textAlign: 'center',
+                            fontSize: '1.2rem' 
+                        }}>
                         배송비 <br />
                         <strong>{shippingCost.toLocaleString()}원</strong>
                     </Typography>
-                    <Divider orientation="vertical" flexItem sx={{ marginRight: '100px', borderColor: '#ddd' }} />
-                    <Typography sx={{ textAlign: 'center' }}>
+                    <Divider 
+                        orientation="vertical" 
+                        flexItem 
+                        sx={{ 
+                            marginRight: '100px', 
+                            borderColor: '#ddd' 
+                        }} 
+                    />
+                    <Typography 
+                        sx={{ 
+                            textAlign: 'center',
+                            fontSize: '1.2rem' 
+                        }}>
                         결제예정금액 <br />
                         <strong>{totalAmount.toLocaleString()}원</strong>
                     </Typography>
                 </Box>
                 <Link to="/order">
-                    <Button variant="contained"
+                    <Button 
+                        variant="contained"
                         sx={{ 
                             backgroundColor: '#7B52E1', 
                             color: 'white', 
                             padding: '10px 100px',  
                             marginRight: '200px',
-                            fontSize: '1.2rem'
+                            fontSize: '1.2rem' 
                         }}>
                         주문하기
                     </Button>
--- dearpet/src/component/CartHeader.js
+++ dearpet/src/component/CartHeader.js
@@ -3,8 +3,19 @@ import { Box, Grid2, Checkbox, Typography, Button } from '@mui/material';
 
 const CartHeader = ({ items, handleSelectAll, handleDeleteSelected }) => {
     return (
-        <Grid2 container alignItems="center" sx={{ mb: 2 }}>
-            <Grid2 item xs={6} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Grid2 
+            container 
+            alignItems="center" 
+            sx={{ 
+                mb: 2 
+            }}>
+            <Grid2 
+                item 
+                xs={6} 
+                sx={{ 
+                    display: 'flex', 
+                    alignItems: 'center' 
+                }}>
                 <Checkbox
                     checked={items.every((item) => item.checked)}
                     indeterminate={
@@ -12,23 +23,39 @@ const CartHeader = ({ items, handleSelectAll, handleDeleteSelected }) => {
                     }
                     onChange={(e) => handleSelectAll(e.target.checked)}
                 />
-                <Typography variant="subtitle1">
+                <Typography 
+                    variant="subtitle1"
+                    sx={{ 
+                        fontSize: '1.1rem' 
+                    }}>
                     전체 선택({items.filter((item) => item.checked).length}/{items.length})
                 </Typography>
             </Grid2>
-            <Grid2 item xs={6} sx={{ textAlign: 'right' }}>
+            <Grid2 
+                item 
+                xs={6} 
+                sx={{ 
+                    textAlign: 'right',
+                    marginLeft: '60px' 
+                }}>
                 <Button 
                     onClick={handleDeleteSelected} 
                     sx={{ 
                         color: 'black', 
-                        marginLeft: 100, 
+                        marginLeft: 100,
+                        fontSize: '1.1rem',
                         backgroundColor: 'transparent', 
                         boxShadow: 'none', 
-                        '&:hover': { backgroundColor: 'transparent' }, 
-                        '&:active': { backgroundColor: 'transparent' }, 
-                        '&:focus': { backgroundColor: 'transparent' } 
-                    }}
-                >
+                        '&:hover': { 
+                            backgroundColor: 'transparent' 
+                        }, 
+                        '&:active': { 
+                            backgroundColor: 'transparent' 
+                        }, 
+                        '&:focus': { 
+                            backgroundColor: 'transparent' 
+                        } 
+                    }}>
                     선택 삭제
                 </Button>
             </Grid2>
@@ -37,3 +64,4 @@ const CartHeader = ({ items, handleSelectAll, handleDeleteSelected }) => {
 };
 
 export default CartHeader;
+

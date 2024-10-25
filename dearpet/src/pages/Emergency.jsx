22df79cf162b967c1e64f8a2d09c3bc5f3ebe5e6
KangBoSeok <kbs9901@gmail.com>
2024-10-25 16:41:26 +0900

    Merge develop into boseok

--- dearpet/src/pages/Emergency.jsx
+++ dearpet/src/pages/Emergency.jsx
@@ -1,22 +1,12 @@
 import React, { useRef, useState } from "react";
-import { Container, Typography, Box, IconButton, Card, CardContent, CardMedia, Grid, Modal } from '@mui/material';
+import { Container, Typography, Box, IconButton, Card, CardContent, CardMedia, Grid, Modal  } from '@mui/material';
 import { ChevronLeft, ChevronRight } from '@mui/icons-material';
 import '../style/Emergency.css';
 import Header from "../component/Header";
 import SearchBar from "../component/SearchBar";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import videoData from '../data/videoData';
 import emergencyGuides from '../data/emergencyGuides';
 import emergencyDefault from '../images/emergencyDefault.png'
-=======
-import CloseIcon from '@mui/icons-material/Close';
-import emergencyGuides from '../data/emergencyGuides'; 
-import videoData from '../data/videodata'; // 비디오 데이터 임포트
->>>>>>> 8aa4756 (Feat: 데이터파일생성)
-=======
-import CloseIcon from '@mui/icons-material/Close';
->>>>>>> 2f0f238bf5fc6c803a61a55a9b87bdda1da82ab4
 
 export default function Emergency() {
     const [open, setOpen] = useState(false); // 모달 열림 상태 관리
@@ -29,9 +19,7 @@ export default function Emergency() {
     };
 
     const handleClose = () => {
-        setOpen(false);
-    };
-
+        setOpen(false);    };
     const scrollRef = useRef(null);
     const [startIndex, setStartIndex] = useState(0); // 현재 보이는 시작 인덱스
 
@@ -53,7 +41,7 @@ export default function Emergency() {
 
     return (
         <div>
-<<<<<<< HEAD
+
         <Header/>
         <Container maxWidth={false} style={{ textAlign: 'center', padding: '20px' }}>
             <h1>반려동물 응급처치 가이드</h1>
@@ -90,6 +78,12 @@ export default function Emergency() {
                         반려동물 건강정보
                     </Typography>
                 </Box>
+                {filteredVideos.length === 0 ? (
+                        <Typography variant="h6" sx={{ margin: 'auto', padding: '110px' }}>
+                            검색 결과가 없습니다.
+                        </Typography>
+                    ) : (
+                <>
                 <IconButton 
                     onClick={() => scroll('left')} 
                     disabled={startIndex === 0} // 시작 인덱스가 0일 때 비활성화
@@ -100,35 +94,13 @@ export default function Emergency() {
                 <Box 
                     ref={scrollRef} 
                     sx={{ 
-=======
-            <Header />
-            <Container maxWidth={false} style={{ textAlign: 'center', padding: '20px' }}>
-                <h1>반려동물 응급처치 가이드</h1>
-                <div className="search">
-                    <SearchBar />
-                </div>
-                <Box 
-                    sx={{ 
-                        backgroundColor: "#F7F4FD",
-                        position: 'relative', 
->>>>>>> 8aa4756 (Feat: 데이터파일생성)
                         display: 'flex', 
-                        alignItems: 'center', 
-                        border: '2px solid #E0D7F8', 
-                        borderRadius: '10px', 
-                        overflow: 'hidden', 
-                        width: '80%', 
-                        maxWidth: '1300px', 
-                        margin: '0 auto', 
-                        padding: '10px' 
+                        scrollBehavior: 'smooth', 
+                        width: '100%', // 부모 박스 크기 설정
+                        paddingTop: '40px', // 제목 아래 여백 추가
                     }}
                 >
-<<<<<<< HEAD
-<<<<<<< HEAD
                     {filteredVideos.slice(startIndex, startIndex + 4).map((video, index) => ( // 현재 보이는 인덱스 범위만 표시
-=======
-                    {videoData.slice(startIndex, startIndex + 4).map((video, index) => ( // 현재 보이는 인덱스 범위만 표시
->>>>>>> 2f0f238bf5fc6c803a61a55a9b87bdda1da82ab4
                         <Box 
                             key={index} 
                             sx={{ 
@@ -151,92 +123,16 @@ export default function Emergency() {
                             </Typography>
                         </Box>
                     ))}
-=======
-                    <Box 
-                        sx={{ 
-                            position: 'absolute', 
-                            top: '10px', 
-                            left: '10px', 
-                            zIndex: 1, 
-                            padding: '5px 10px', 
-                            backgroundColor: '#F7F4FD', 
-                            borderRadius: '5px' 
-                        }}
-                    >
-                        <Typography variant="h6" style={{ fontWeight: "bold" }}>
-                            반려동물 건강정보
-                        </Typography>
-                    </Box>
-                    <IconButton 
-                        onClick={() => scroll('left')} 
-                        disabled={startIndex === 0} 
-                        style={{ position: 'absolute', left: '2px', zIndex: 1 }}
-                    >
-                        <ChevronLeft />
-                    </IconButton>
-                    <Box 
-                        ref={scrollRef} 
-                        sx={{ 
-                            display: 'flex', 
-                            scrollBehavior: 'smooth', 
-                            width: '100%', 
-                            paddingTop: '40px', 
-                        }}
-                    >
-                        {videoData.slice(startIndex, startIndex + 4).map((video, index) => (
-                            <Box 
-                                key={index} 
-                                sx={{ 
-                                    marginTop: '20px',
-                                    marginRight: '15px',
-                                    marginLeft: '15px', 
-                                    cursor: 'pointer', 
-                                    width: '300px', 
-                                    height: 'auto' 
-                                }} 
-                                onClick={() => window.open(video.link, '_blank')}
-                            >
-                                <img 
-                                    src={video.thumbnail} 
-                                    alt={video.title} 
-                                    style={{ width: '250px', height: 'auto', borderRadius: '10px' }} 
-                                />
-                                <Typography variant="subtitle1" style={{ marginTop: '5px', fontWeight: 'bold' }}>
-                                    {video.title}
-                                </Typography>
-                            </Box>
-                        ))}
-                    </Box>
-                    <IconButton 
-                        onClick={() => scroll('right')} 
-                        disabled={startIndex >= videoData.length - 4} 
-                        style={{ position: 'absolute', right: '1px', zIndex: 1 }} 
-                    >
-                        <ChevronRight />
-                    </IconButton>
->>>>>>> 8aa4756 (Feat: 데이터파일생성)
                 </Box>
-            </Container>
-            <Container maxWidth={false} style={{ textAlign: 'center', padding: '20px' }}>
-                <Box
-                    sx={{
-                        backgroundColor: "#F7F4FD",
-                        position: 'relative',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'flex-start',
-                        border: '2px solid #E0D7F8',
-                        borderRadius: '10px',
-                        overflow: 'hidden',
-                        width: '80%',
-                        maxWidth: '1300px',
-                        margin: '0 auto',
-                        padding: '10px'
-                    }}
+                <IconButton 
+                    onClick={() => scroll('right')} 
+                    disabled={startIndex >= videoData.length - 4} // 마지막 인덱스에서 비활성화
+                    style={{ position: 'absolute', right: '1px', zIndex: 1 }} // 오른쪽 화살표 위치 조정
                 >
-<<<<<<< HEAD
                     <ChevronRight />
                 </IconButton>
+            </>
+            )}
             </Box>
         </Container>
         <Container maxWidth={false} style={{ textAlign: 'center', padding: '20px' }}>
@@ -255,107 +151,35 @@ export default function Emergency() {
                     margin: '0 auto',
                     padding: '10px'
                 }}
-=======
-                    <Box
-                        sx={{
-                            position: 'absolute',
-                            top: '10px',
-                            left: '10px',
-                            zIndex: 1,
-                            padding: '5px 10px',
-                            backgroundColor: '#F7F4FD',
-                            borderRadius: '5px'
-                        }}
-                    >
-                        <Typography variant="h6" style={{ fontWeight: "bold" }}>
-                            반려동물 질병정보
-                        </Typography>
-                    </Box>
-
-                    {/* 응급처치 가이드 카드 목록 */}
-                    <Grid container spacing={3} sx={{ paddingTop: '60px' }}>
-                        {emergencyGuides.map((guide, index) => (
-                            <Grid item xs={12} sm={6} md={3} key={index}>
-                                <Card sx={{ height: '100%' }} onClick={() => handleOpen(guide)}>
-                                    <CardMedia
-                                        component="img"
-                                        height="140"
-                                        image={guide.image}
-                                        alt={guide.title}
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h6" component="div">
-                                            {guide.title}
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary">
-                                            {guide.description}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Box>
-            </Container>
-
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-title"
-                aria-describedby="modal-description"
->>>>>>> 8aa4756 (Feat: 데이터파일생성)
             >
                 <Box
                     sx={{
                         position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        width: 400,
-                        bgcolor: '#F7F4FD',
-                        border: '2px solid #E0D7F8',
-                        boxShadow: 24,
-                        borderRadius: '10px',
-                        p: 4,
+                        top: '10px',
+                        left: '10px',
+                        zIndex: 1,
+                        padding: '5px 10px',
+                        backgroundColor: '#F7F4FD',
+                        borderRadius: '5px'
                     }}
                 >
-                    <IconButton
-                        sx={{ position: 'absolute', top: 8, right: 8 }}
-                        onClick={handleClose}
-                    >
-                        <CloseIcon />
-                    </IconButton>
-
-                    {selectedGuide && (
-                        <>
-                            <Typography id="modal-title" variant="h6" component="h2">
-                                {selectedGuide.title}
-                            </Typography>
-                            <Typography id="modal-description" sx={{ mt: 2 }}>
-                                {selectedGuide.detail}
-                            </Typography>
-                        </>
-                    )}
+                    <Typography variant="h6" style={{ fontWeight: "bold" }}>
+                        반려동물 질병정보
+                    </Typography>
                 </Box>
-<<<<<<< HEAD
-<<<<<<< HEAD
                 {filteredGuides.length === 0 ? (
                         <Typography variant="h6" sx={{ margin: 'auto', padding: '110px' }}>
                             검색 결과가 없습니다.
                         </Typography>
                     ) : (
-=======
-
-                {/* 응급처치 가이드 카드 목록 */}
->>>>>>> 2f0f238bf5fc6c803a61a55a9b87bdda1da82ab4
                 <Grid container spacing={3} sx={{ paddingTop: '60px' }}>
-                    {emergencyGuides.map((guide, index) => (
+                    {filteredGuides.map((guide, index) => (
                         <Grid item xs={12} sm={6} md={3} key={index}>
                             <Card sx={{ height: '100%' }} onClick={() => handleOpen(guide)}>
                                 <CardMedia
                                     component="img"
                                     height="140"
-                                    image={guide.image}
+                                    image={emergencyDefault}
                                     alt={guide.title}
                                 />
                                 <CardContent>
@@ -370,16 +194,30 @@ export default function Emergency() {
                         </Grid>
                     ))}
                 </Grid>
+                )}
             </Box>
         </Container>
 
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-title"
-                aria-describedby="modal-description"
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-title"
+            aria-describedby="modal-description"
+        >
+            <Box
+                sx={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: 400,
+                    bgcolor: '#F7F4FD', // Set the background color to match your theme
+                    border: '2px solid #E0D7F8', // Border color to match your theme
+                    boxShadow: 24,
+                    borderRadius: '10px', // Add rounded corners
+                    p: 4,
+                }}
             >
-<<<<<<< HEAD
                 {selectedGuide && (
                     <>
                         <Typography id="modal-title" variant="h6" component="h2">
@@ -392,44 +230,6 @@ export default function Emergency() {
                 )}
             </Box>
         </Modal>
-=======
-            </Modal>
->>>>>>> 8aa4756 (Feat: 데이터파일생성)
-=======
-                <Box
-                    sx={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        width: 400,
-                        bgcolor: '#F7F4FD',
-                        border: '2px solid #E0D7F8',
-                        boxShadow: 24,
-                        borderRadius: '10px',
-                        p: 4,
-                    }}
-                >
-                    <IconButton
-                        sx={{ position: 'absolute', top: 8, right: 8 }}
-                        onClick={handleClose}
-                    >
-                        <CloseIcon />
-                    </IconButton>
-
-                    {selectedGuide && (
-                        <>
-                            <Typography id="modal-title" variant="h6" component="h2">
-                                {selectedGuide.title}
-                            </Typography>
-                            <Typography id="modal-description" sx={{ mt: 2 }}>
-                                {selectedGuide.detail}
-                            </Typography>
-                        </>
-                    )}
-                </Box>
-            </Modal>
->>>>>>> 2f0f238bf5fc6c803a61a55a9b87bdda1da82ab4
         </div>
     );
 }

9fb1f5fff967e54197c6dd8e97d69d7561a3c8a0
Sanghee Park <tkdgml1241@naver.com>
2024-10-29 10:58:18 +0900

    로그인 오류해결, 주문내역 productId추가

--- dearpet/src/pages/Login.jsx
+++ dearpet/src/pages/Login.jsx
@@ -7,14 +7,15 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import '../style/Login.css';  // 스타일을 위한 CSS 파일
+import '../style/Login.css'; 
 import boneLogo from '../images/bone.png';
+
 // Material UI 테마
 const theme = createTheme({
   typography: {
     allVariants: {
-      color: '#000', // 모든 텍스트 색상을 검은색으로 설정
-      fontWeight: 'normal', // 모든 텍스트 굵기 제거
+      color: '#000',
+      fontWeight: 'normal', 
     },
   },
   components: {
@@ -24,35 +25,39 @@ const theme = createTheme({
           backgroundColor: '#000',
           color: '#fff',
           '&:hover': {
-            backgroundColor: '#333', // 버튼 hover 시 어두운 색상으로 변경
+            backgroundColor: '#333', 
           },
         },
         outlined: {
           color: '#000',
           borderColor: '#000',
           '&:hover': {
-            borderColor: '#333', // outlined 버튼 hover 시 테두리 색상 변경
-            backgroundColor: '#f5f5f5', // hover 시 배경색 변경
+            borderColor: '#333',
+            backgroundColor: '#f5f5f5', 
           },
         },
       },
     },
   },
 });
+
 export default function Login({ setIsLoggedIn, setUserId }) {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(""); // 에러 메시지 상태 추가
+  const [errorMessage, setErrorMessage] = useState(""); 
+  const navigate = useNavigate();
 
   // 로컬 로그인 처리
   const handleSubmit = async (event) => {
     event.preventDefault();
+    
     const userData = {
       username: id,
       password: password,
     };
+
     try {
-      const response = await fetch("http://localhost:8080/api/users/login", {
+      const response = await fetch("http://localhost:8080/api/auth/login", { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -60,24 +65,29 @@ export default function Login({ setIsLoggedIn, setUserId }) {
         body: JSON.stringify(userData),
       });
 
-      const textResponse = await response.text(); 
-
       if (response.ok) {
-        const token = textResponse;  // JWT 토큰 문자열 처리
-        console.log('Login successful:', token);
-        localStorage.setItem('token', token);  // 로그인 토큰 저장
-        localStorage.setItem('isLoggedIn', true);  // 로그인 상태 저장
+        const token = await response.text(); 
+        localStorage.setItem('token', token); 
+        localStorage.setItem('isLoggedIn', true);
         localStorage.setItem('userId', id);
-        setIsLoggedIn(true);  // 상태 업데이트
+        setIsLoggedIn(true); 
+        setUserId(id);
 
+        navigate("/");
       } else {
-        console.error('Login failed:', textResponse);
+        const errorText = await response.text();
+        setErrorMessage("로그인에 실패했습니다. 다시 시도해 주세요."); 
+        console.error('Login failed:', errorText);
       }
+      
     } catch (error) {
       console.error('Error:', error);
       setErrorMessage("서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.");
     }
   };
+
+
+
   return (
     <div className="main-content">
       <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
@@ -85,7 +95,7 @@ export default function Login({ setIsLoggedIn, setUserId }) {
               CarePet
               <img style={{width: '40px'}} src={boneLogo} alt='로고'></img>
             </Typography>
-      </Link>
+      </Link>  
       <ThemeProvider theme={theme}>
         <Container component="main" maxWidth="xs" className="login-container">
           <CssBaseline />
@@ -94,7 +104,7 @@ export default function Login({ setIsLoggedIn, setUserId }) {
               marginTop: 4,
               display: 'flex',
               flexDirection: 'column',
-              alignItems: 'center', // 가운데 정렬
+              alignItems: 'center', 
             }}
           >
             <Typography
@@ -125,6 +135,7 @@ export default function Login({ setIsLoggedIn, setUserId }) {
                   },
                 }}}
               />
+
             <Typography
               variant="body1"
               sx={{ alignSelf: 'flex-start', mb: 0, fontWeight: 'bold', textShadow: 'none', fontSize:'1.5rem' }}
@@ -179,6 +190,7 @@ export default function Login({ setIsLoggedIn, setUserId }) {
             >
               <div style={{fontSize:"1.2rem"}}>구글로 시작하기</div>
             </Button>
+
             <Button
               fullWidth
               variant="outlined"
@@ -191,7 +203,7 @@ export default function Login({ setIsLoggedIn, setUserId }) {
               />}
               sx={{ mt: 2, mb: 2 }}
             >
-              <div style={{fontSize:"1.2rem"}}>카카오톡으로 시작하기</div>
+               <div style={{fontSize:"1.2rem"}}>카카오톡으로 시작하기</div>
             </Button>
             <Typography
               variant="body2"
